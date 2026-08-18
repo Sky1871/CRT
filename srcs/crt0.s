d92bf5405d61007954c09024bc22f4e0a7dd8774
ENBY <seltenreichkristof@gmail.com>
2026-08-18 03:44:32 +0200

    no comment

--- srcs/crt0.s
+++ srcs/crt0.s
@@ -1,6 +1,6 @@
 .global _start
 
 _start:
-	mov %rsp, %rdi // Copies the value of the stack pointer into the first argument register
-	and $-16, %rsp // Aligns the stack pointer to a 16-byte boundary
+	mov %rsp, %rdi
+	and $-16, %rsp
 	call crt_init
